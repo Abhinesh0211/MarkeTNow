99ce72889d2043864b91857b35e25f48104e3256
GauthamMurali <sec21cj016@sairamtap.edu.in>
2025-06-16 15:32:30 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './my-style.scss';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </h2>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
