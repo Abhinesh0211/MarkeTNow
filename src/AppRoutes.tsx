b78d3e367f15ddfb71a894c35370b715b739e036
Lakshmi <srlaaxmi@gmail.com>
2025-06-27 11:42:22 +0530

    push

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -11,6 +11,9 @@ import UserLayout from './layouts/UserLayout';
 import MarketOverviewCard from './pages/MarketOverview'; 
 import SigninForm from './pages/SigninForm';
 import SignupForm from './pages/SignupForm';
+import { CurrencyExchange, Dashboard } from '@mui/icons-material';
+import Footer from './components/Footer';
+import Stocks from './pages/Stocks';
 // ✅ Correct
 
 
@@ -19,13 +22,29 @@ import SignupForm from './pages/SignupForm';
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<Landing />} />
+      <Route path="/" element={
+        <>
+        <Landing />
+        <Footer/>
+        </>
+        } />
       <Route path="/signin" element={<SigninForm />} />
       <Route path="/signup" element={<SignupForm />} />
 
       <Route element={<UserLayout />}>
-        <Route path="home" element={<Home />} />
+        <Route 
+        path="home"
+         element={
+         <>
+         < Home />
+         <Category />
+         </>
+         }
+        /> 
+        <Route path="currencies" element ={<CurrencyExchange/>}/>
+        <Route path="stocks" element ={<Stocks/>}/>
         <Route path="marketoverviewcard" element={<MarketOverviewCard />} />
+        <Route path="dashboard" element={<Dashboard />} />
         <Route path="newspage" element={<NewsPage />} />
         <Route path="wishlist" element={<Wishlist />} />
         <Route path="category" element={<Category />} />
